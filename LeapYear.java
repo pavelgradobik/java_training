170796d291237567bea3bedf3d2bc291b508937c
pavelgradobik <gradobik@gmail.com>
2016-01-17 18:58:57 +0200

    Create LeapYear.java

---
+++ LeapYear.java
@@ -0,0 +1,25 @@
+package homeworkLevel1;
+
+import java.util.Scanner;
+
+public class LeapYear {
+	public static void main(String[] args){
+		
+		Scanner scan = new Scanner(System.in);
+		
+		System.out.println("введите год");
+		
+		int year = scan.nextInt();
+		
+		if((year % 4 == 0) & (year % 100 != 0) | (year % 400 == 0) ){
+			
+			System.out.println("Високосный");
+			
+			
+		} else {
+			System.out.println("Невисокосный");
+		}
+		
+	}
+
+}
